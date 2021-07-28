1bda1c2c712d5acf4a093e52159b10282a3789d2
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-07-28 15:24:18 +0530

    Time: 80 ms (56.77%), Space: 14.5 MB (24.48%) - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        vector<int> dp(amount+1, INT_MAX);
+        
+        dp[0] = 0;
+        
+        sort(coins.begin(), coins.end());
+        
+        for(int i=1; i<=amount; i++){
+            
+            for(int coin : coins){
+                if(i>=coin && dp[i-coin] != INT_MAX){
+                    dp[i] = min(dp[i], 1+dp[i-coin]);
+                }
+            }
+            
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        
+    }
+};

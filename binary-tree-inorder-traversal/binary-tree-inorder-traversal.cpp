9d51421d48f34fd58e48c580d88b8daf3c39ef2b
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-08-04 14:27:59 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (61.63%) - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> res;
+    
+    void helper(TreeNode* root){
+        if(!root) return;
+        
+        helper(root->left);
+        res.push_back(root->val);
+        helper(root->right);
+        
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        helper(root);
+        return res;
+    }
+};

96ee7f2c294bfa3a0f4be1f8c8bab951ce429324
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-08-04 11:58:15 +0530

    Time: 20 ms (53.29%), Space: 19.5 MB (86.80%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        return nums[n/2];
+    }
+};

83f28488b1305ab76224d141fd39242950786bbe
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-09-04 21:26:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (54.81%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int arr[n+1];
+        
+        arr[0] = 1;
+        arr[1] = 1;
+        
+        for(int i = 2; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return arr[n];
+        
+    }
+};

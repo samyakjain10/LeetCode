a5cbaf695828c091d27413116dc27f74650e6416
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-09-18 22:04:39 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.54%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(auto i=0;i<numRows;++i)
+        {
+            res.push_back(vector<int>(i+1,1));
+            for(auto j=1; j<i; ++j) res[i][j] = res[i-1][j-1] + res[i-1][j];
+        }
+        return res;
+    }
+};

4db7ff17bd0b5aef5b810090949bc864a13728dc
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-09-22 22:42:49 +0530

    Time: 24 ms (74.60%), Space: 11.9 MB (45.18%) - LeetHub

---
+++ maximal-square/maximal-square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!i || !j || matrix[i][j] == '0') {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                sz = max(dp[i][j], sz);
+            }
+        }
+        return sz * sz;
+    }
+};

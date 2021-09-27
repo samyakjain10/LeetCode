bac0175d5dbb6332bc8a5686cdc88a0f0553bb4f
Samyak Jain <56185041+samyakjain10@users.noreply.github.com>
2021-09-27 10:56:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (44.26%) - LeetHub

---
+++ integer-break/integer-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n <= 2)
+            return 1;
+
+        vector<int> maxArr(n+1, 0);
+        
+        maxArr[1] = 0;
+        maxArr[2] = 1;
+        
+        for (int i=3; i<=n; i++) {
+            for (int j=1; j<i; j++) {
+                
+                maxArr[i] = max({maxArr[i], j*(i-j), j*maxArr[i-j]});
+                
+            }
+        }
+        return maxArr[n];
+    }
+};

98a5cc2d9586ed646a89e5d80b1633e166a9b080
Rishu Yadav <rishuss1212@gmail.com>
2021-10-01 14:45:19 +0530

    Added solution to Construct binary tree from preorder and inorder traversal

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+public:
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+    
+    }
+    TreeNode* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj)
+    {
+        if(i >= j || ii >= j)
+            return NULL;
+
+        int mid = preorder[i];
+        auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
+
+        int dis = f - inorder.begin() - ii;
+
+        TreeNode* root = new TreeNode(mid);
+        root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
+        root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
+        return root;
+    }
+};
